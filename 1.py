c1b910c08179fd411e3c24864a1695255b78db53
Akash Padmane <akashspadmane@gmail.com>
2016-03-12 00:09:27 +0530

    1.py

---
+++ 1.py
@@ -0,0 +1,64 @@
+
+        
+import string
+upper = list(string.ascii_uppercase)
+lower = list(string.ascii_lowercase)
+nos = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+spcl = ['!', '@','#','$','%','^','&','*','(' ,')']
+total = upper + lower + nos + spcl
+print (len(total))
+o = 0
+bool b1 = false
+bool b2 = false
+
+
+
+     
+for i in range(len(total)):
+    a =total[i]
+    for j in range(len(total)):
+        b = total[j]
+        for k in range(len(total)):
+            c = total[k]
+            for l in range(len(total)):
+                d = total[l]
+                for m in range(len(total)):
+                    e = total[m]
+                    for n in range(len(total)):
+                        f = total[n]
+                        output = (a+b+c+d+e+f)
+                        if(len(output)==6) b1 = true
+                        
+                            
+                     
+                       
+                    
+                    
+                                             
+
+
+
+
+
+
+                        
+                        
+                        
+        
+       
+        
+       
+
+        
+   
+        
+
+      
+    
+
+      
+
+
+
+
+      
